daae2d37c37a2bddada424d7805cda0652590a83
sp1aca9fa <94608018+sp1aca9fa@users.noreply.github.com>
2024-06-23 23:39:09 +0900

    JavaScript - Building a Spam Filter (from start until 23)

---
+++ JavaScript Algorithms and Data Structures (Beta) Certification/Learn Regular Expressions by Building a Spam Filter/README.md
@@ -0,0 +1,5 @@
+Learn Regular Expressions by Building a Spam Filter
+
+All of the HTML and CSS for this project has been provided by freeCodeCamp.
+
+JavaScript has been built following the course/instructions on freeCodeCamp.
---
+++ JavaScript Algorithms and Data Structures (Beta) Certification/Learn Regular Expressions by Building a Spam Filter/script.js
@@ -0,0 +1,23 @@
+const messageInput = document.getElementById("message-input");
+const result = document.getElementById("result");
+const checkMessageButton = document.getElementById("check-message-btn");
+
+const helpRegex = /please help|assist me/i;
+const dollarRegex = /[0-9]+ (?:hundred|thousand|million|billion)? dollars/i;
+const freeRegex = /fr[e3][e3] mon[e3]y/i;
+
+const denyList = [helpRegex, dollarRegex, freeRegex];
+
+const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
+
+checkMessageButton.addEventListener("click", () => {
+  if (messageInput.value === "") {
+    alert("Please enter a message.");
+    return;
+  }
+
+  result.textContent = isSpam(messageInput.value)
+    ? "Oh no! This looks like a spam message."
+    : "This message does not seem to contain any spam.";
+  messageInput.value = "";
+});
