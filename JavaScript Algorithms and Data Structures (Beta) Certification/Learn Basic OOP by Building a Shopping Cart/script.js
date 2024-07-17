64c37d593af8d9ae3917ce6c532b901264d39aac
sp1aca9fa <94608018+sp1aca9fa@users.noreply.github.com>
2024-07-17 23:24:56 +0900

    JavaScript - Build a Shopping Cart (until step 25)

--- JavaScript Algorithms and Data Structures (Beta) Certification/Learn Basic OOP by Building a Shopping Cart/script.js
+++ JavaScript Algorithms and Data Structures (Beta) Certification/Learn Basic OOP by Building a Shopping Cart/script.js
@@ -99,4 +99,24 @@ products.forEach(({ name, id, price, category }) => {
     `;
 });
 
-class ShoppingCart {}
+class ShoppingCart {
+  constructor() {
+    this.items = [];
+    this.total = 0;
+    this.taxRate = 8.25;
+  }
+
+  addItem(id, products) {
+    const product = products.find((item) => item.id === id);
+    const { name, price } = product;
+    this.items.push(product);
+
+    const totalCountPerProduct = {};
+    this.items.forEach((dessert) => {
+      totalCountPerProduct[dessert.id] =
+        (totalCountPerProduct[dessert.id] || 0) + 1;
+    });
+
+    const currentProductCount = totalCountPerProduct[product.id];
+  }
+}
