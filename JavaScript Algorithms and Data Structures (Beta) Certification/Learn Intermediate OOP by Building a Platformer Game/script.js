2d5adbeba6dcde7dbd9468e3d6f075c9d7397916
sp1aca9fa <94608018+sp1aca9fa@users.noreply.github.com>
2024-07-23 01:01:30 +0900

    JavaScript - Build a platformer game (until step 26)

--- JavaScript Algorithms and Data Structures (Beta) Certification/Learn Intermediate OOP by Building a Platformer Game/script.js
+++ JavaScript Algorithms and Data Structures (Beta) Certification/Learn Intermediate OOP by Building a Platformer Game/script.js
@@ -23,5 +23,22 @@ class Player {
       x: 0,
       y: 0,
     };
+    this.width = proportionalSize(40);
+    this.height = proportionalSize(40);
+  }
+  draw() {
+    ctx.fillStyle = "#99c9ff";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+      if (this.position.y < 0) {
+      }
+    }
   }
 }
