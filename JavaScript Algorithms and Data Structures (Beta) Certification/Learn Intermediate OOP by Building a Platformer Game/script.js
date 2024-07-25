70c859342cc94eef9fdde3fb22e9ad4b825a2c59
sp1aca9fa <94608018+sp1aca9fa@users.noreply.github.com>
2024-07-26 00:11:36 +0900

    JavaScript - Build a platformer game (until step 56)

--- JavaScript Algorithms and Data Structures (Beta) Certification/Learn Intermediate OOP by Building a Platformer Game/script.js
+++ JavaScript Algorithms and Data Structures (Beta) Certification/Learn Intermediate OOP by Building a Platformer Game/script.js
@@ -64,6 +64,14 @@ const animate = () => {
   player.update();
 
   if (keys.rightKey.pressed && player.position.x < proportionalSize(400)) {
+    player.velocity.x = 5;
+  } else if (
+    keys.leftKey.pressed &&
+    player.position.x > proportionalSize(100)
+  ) {
+    player.velocity.x = -5;
+  } else {
+    player.velocity.x = 0;
   }
 };
 
@@ -74,6 +82,24 @@ const keys = {
   leftKey: {
     pressed: false,
   },
+};
+
+const movePlayer = (key, xVelocity, isPressed) => {
+  if (!isCheckpointCollisionDetectionActive) {
+    player.velocity.x = 0;
+    player.velocity.y = 0;
+    return;
+  }
+
+  switch (key) {
+    case "ArrowLeft":
+      keys.leftKey.pressed = isPressed;
+      if (xVelocity === 0) {
+        player.velocity.x = xVelocity;
+      }
+      player.velocity.x -= xVelocity;
+      break;
+  }
 };
 
 const startGame = () => {
