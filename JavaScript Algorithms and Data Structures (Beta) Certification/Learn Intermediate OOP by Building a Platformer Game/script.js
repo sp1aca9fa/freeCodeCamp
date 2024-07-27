fa9bb0536e4381b04fd56f0e54628ed9f30fe503
sp1aca9fa <94608018+sp1aca9fa@users.noreply.github.com>
2024-07-28 02:15:09 +0900

    JavaScript - Build a platformer game (until step 76)

--- JavaScript Algorithms and Data Structures (Beta) Certification/Learn Intermediate OOP by Building a Platformer Game/script.js
+++ JavaScript Algorithms and Data Structures (Beta) Certification/Learn Intermediate OOP by Building a Platformer Game/script.js
@@ -62,14 +62,40 @@ class Platform {
       x,
       y,
     };
+    this.width = 200;
+    this.height = proportionalSize(40);
+  }
+  draw() {
+    ctx.fillStyle = "#acd157";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 }
 
 const player = new Player();
 
+const platformPositions = [
+  { x: 500, y: proportionalSize(450) },
+  { x: 700, y: proportionalSize(400) },
+  { x: 850, y: proportionalSize(350) },
+  { x: 900, y: proportionalSize(350) },
+  { x: 1050, y: proportionalSize(150) },
+  { x: 2500, y: proportionalSize(450) },
+  { x: 2900, y: proportionalSize(400) },
+  { x: 3150, y: proportionalSize(350) },
+  { x: 3900, y: proportionalSize(450) },
+  { x: 4200, y: proportionalSize(400) },
+  { x: 4400, y: proportionalSize(200) },
+  { x: 4700, y: proportionalSize(150) },
+];
+
+const platforms = platformPositions.map(
+  (platform) => new Platform(platform.x, platform.y)
+);
+
 const animate = () => {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   player.update();
 
   if (keys.rightKey.pressed && player.position.x < proportionalSize(400)) {
