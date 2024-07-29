f9c2ec3e2dc3465f1e4cfe5dc02be7f3f90c63b0
sp1aca9fa <94608018+sp1aca9fa@users.noreply.github.com>
2024-07-29 13:32:29 +0900

    JavaScript - Build a platformer game (until step 86)

--- JavaScript Algorithms and Data Structures (Beta) Certification/Learn Intermediate OOP by Building a Platformer Game/script.js
+++ JavaScript Algorithms and Data Structures (Beta) Certification/Learn Intermediate OOP by Building a Platformer Game/script.js
@@ -96,6 +96,10 @@ const animate = () => {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  platforms.forEach((platform) => {
+    platform.draw();
+  });
+
   player.update();
 
   if (keys.rightKey.pressed && player.position.x < proportionalSize(400)) {
@@ -107,7 +111,35 @@ const animate = () => {
     player.velocity.x = -5;
   } else {
     player.velocity.x = 0;
+
+    if (keys.rightKey.pressed && isCheckpointCollisionDetectionActive) {
+      platforms.forEach((platform) => {
+        platform.position.x -= 5;
+      });
+    } else if (keys.leftKey.pressed && isCheckpointCollisionDetectionActive) {
+      platforms.forEach((platform) => {
+        platform.position.x += 5;
+      });
+    }
   }
+
+  platforms.forEach((platform) => {
+    const collisionDetectionRules = [
+      player.position.y + player.height <= platform.position.y,
+      player.position.y + player.height + player.velocity.y >=
+        platform.position.y,
+      player.position.x >= platform.position.x - player.width / 2,
+      player.position.x <=
+        platform.position.x + platform.width - player.width / 3,
+    ];
+
+    if (collisionDetectionRules.every((rule) => rule)) {
+      player.velocity.y = 0;
+      return;
+    }
+
+    const platformDetectionRules = [];
+  });
 };
 
 const keys = {
