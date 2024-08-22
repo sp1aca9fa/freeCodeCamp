babb5a3174640deba8469e48a9e671deb9ab3517
sp1aca9fa <94608018+sp1aca9fa@users.noreply.github.com>
2024-08-23 00:34:14 +0900

    JavaScript - Build a forum leaderboard (until step 34)

--- JavaScript Algorithms and Data Structures (Beta) Certification/Learn Asynchronous Programming by Building an fCC Forum Leaderboard/script.js
+++ JavaScript Algorithms and Data Structures (Beta) Certification/Learn Asynchronous Programming by Building an fCC Forum Leaderboard/script.js
@@ -6,6 +6,25 @@ const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 const postsContainer = document.getElementById("posts-container");
 
+const allCategories = {
+  299: { category: "Career Advice", className: "career" },
+  409: { category: "Project Feedback", className: "feedback" },
+  417: { category: "freeCodeCamp Support", className: "support" },
+  421: { category: "JavaScript", className: "javascript" },
+  423: { category: "HTML - CSS", className: "html-css" },
+  424: { category: "Python", className: "python" },
+  432: { category: "You Can Do This!", className: "motivation" },
+  560: { category: "Backend Development", className: "backend" },
+};
+
+const forumCategory = (id) => {
+  let selectedCategory = {};
+
+  if (allCategories.hasOwnProperty(id)) {
+    const { className, category } = allCategories[id];
+  }
+};
+
 const timeAgo = (time) => {
   const currentTime = new Date();
   const lastPost = new Date(time);
@@ -28,6 +47,16 @@ const timeAgo = (time) => {
   return `${daysAgo}d ago`;
 };
 
+const viewCount = (views) => {
+  const thousands = Math.floor(views / 1000);
+
+  if (views >= 1000) {
+    return `${thousands}k`;
+  }
+
+  return views;
+};
+
 const fetchData = async () => {
   try {
     const res = await fetch(forumLatest);
@@ -46,16 +75,8 @@ const showLatestPosts = (data) => {
 
   postsContainer.innerHTML = topics
     .map((item) => {
-      const {
-        id,
-        title,
-        views,
-        posts_count,
-        slug,
-        posters,
-        category_id,
-        bumped_at,
-      } = item;
+      const { id, title, views, posts_count, slug, posters, _id, bumped_at } =
+        item;
 
       return `
     <tr>
@@ -64,7 +85,7 @@ const showLatestPosts = (data) => {
       </td>
       <td></td>
       <td>${posts_count - 1}</td>
-      <td>${views}</td>
+      <td>${viewCount(views)}</td>
       <td>${timeAgo(bumped_at)}</td>
     </tr>`;
     })
