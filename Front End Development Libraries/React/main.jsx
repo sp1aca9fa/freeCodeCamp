4af169c571cf7179cc501e3da7463dadf446f882
sp1aca9fa <94608018+sp1aca9fa@users.noreply.github.com>
2024-10-05 03:00:18 +0900

    Front End Development Libraries - React (from start until step 32)

--- Front End Development Libraries/React/main.jsx
+++ Front End Development Libraries/React/main.jsx
@@ -2,19 +2,23 @@ class MyComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'freeCodeCamp'
-    }
+      activeUsers: null
+    };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        activeUsers: 1273
+      });
+    }, 2500);
   }
   render() {
-    // Change code below this line
-    const name = this.state.name
-    // Change code above this line
     return (
       <div>
-        { /* Change code below this line */ }
-        <h1>{name}</h1>
-        { /* Change code above this line */ }
+        {/* Change code below this line */}
+        <h1>Active Users: {this.state.activeUsers}</h1>
+        {/* Change code above this line */}
       </div>
     );
   }
-};
+}
