be12ead24f9c4e9af1777bf2727102a197a3c6a7
sp1aca9fa <94608018+sp1aca9fa@users.noreply.github.com>
2024-10-06 03:45:14 +0900

    Front End Development Libraries - React (from start until step 42)

--- Front End Development Libraries/React/main.jsx
+++ Front End Development Libraries/React/main.jsx
@@ -1,24 +1,30 @@
-class MyComponent extends React.Component {
+class GateKeeper extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeUsers: null
+      input: ''
     };
+    this.handleChange = this.handleChange.bind(this);
   }
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        activeUsers: 1273
-      });
-    }, 2500);
+  handleChange(event) {
+    this.setState({ input: event.target.value })
   }
   render() {
+    let inputStyle = {
+      border: '1px solid black'
+    };
+    // Change code below this line
+    if(this.state.input.length > 15) inputStyle.border = "3px solid red"
+    // Change code above this line
     return (
       <div>
-        {/* Change code below this line */}
-        <h1>Active Users: {this.state.activeUsers}</h1>
-        {/* Change code above this line */}
+        <h3>Don't Type Too Much:</h3>
+        <input
+          type="text"
+          style={inputStyle}
+          value={this.state.input}
+          onChange={this.handleChange} />
       </div>
     );
   }
-}
+};
